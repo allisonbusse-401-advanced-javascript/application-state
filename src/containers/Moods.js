010a11db3c739d54f99974d4425f23e50b668f1a
allisonbusse <allisonbusse@gmail.com>
2019-11-11 15:00:38 -0800

    connected to store

---
+++ src/containers/Moods.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Controls from '../components/controls/Controls';
+import Face from '../components/face/Face';
+import { connect } from 'react-redux';
+
+
+const actions = [
+  { name: 'DRINK_COFFEE', text: 'Drink Coffee', stateName: 'coffees' },
+  { name: 'EAT_SNACK', text: 'Snack', stateName: 'snacks' },
+  { name: 'TAKE_NAP', text: 'Nap', stateName: 'naps' },
+  { name: 'STUDY', text: 'Study', stateName: 'studies' },
+];
+
+export const isTired = state => state.coffees < 1 && state.naps < 1;
+export const isHyper = state => state.coffees > 3;
+export const isEducated = state => state.studies > 2;
+export const isHungry = state => state.snacks < 1;
+
+export const getFace = state => {
+  if(isTired(state) && isHungry(state)) return '🤬';
+  if(isHyper(state) && isHungry(state)) return '🤮';
+  if(isTired(state)) return '😴';
+  if(isHyper(state)) return '🙀';
+  if(isEducated(state)) return '🤯';
+  if(isHungry(state)) return '😡';
+
+  return '😀';
+};
+
+const Moods = ({ count, face, actions, handleSelection }) => {
+  const mappedActions = actions.map(action => ({
+    ...action,
+    count: count[action.stateName]
+  }));
+
+  return (
+    <>
+      <Controls actions={mappedActions} handleSelection={handleSelection} />
+      <Face emoji={face} />
+    </>
+  );
+};
+
+Moods.propTypes = {
+  count: PropTypes.shape(PropTypes.string.isRequired).isRequired,
+  face: PropTypes.string.isRequired,
+  actions: PropTypes.array.isRequired,
+  handleSelection: PropTypes.func.isRequired
+};
+
+
+const mapStateToProps = state => ({
+  count: {
+    coffees: state.coffees,
+    snacks: state.snacks,
+    naps: state.naps,
+    studies: state.studies
+  },
+  face: getFace(state),
+  actions: actions
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleSelection(name) {
+    dispatch({
+      type: name
+    });
+  }
+});
+
+const MoodsContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Moods);
+
+export default MoodsContainer;
+
+// export default class Moods extends Component {
+//   // state = {
+//   //   coffees: 0,
+//   //   snacks: 0,
+//   //   naps: 0,
+//   //   studies: 0
+//   // }
+
+//   handleSelection = name => {
+//     switch(name) {
+//       case 'DRINK_COFFEE':
+//         this.setState(state => ({ coffees: state.coffees + 1 }));
+//         break;
+//       case 'EAT_SNACK':
+//         this.setState(state => ({ snacks: state.snacks + 1 }));
+//         break;
+//       case 'TAKE_NAP':
+//         this.setState(state => ({ naps: state.naps + 1 }));
+//         break;
+//       case 'STUDY':
+//         this.setState(state => ({ studies: state.studies + 1 }));
+//         break;
+//       default:
+//         console.log(`unhandled name: ${name}`);
+//     }
+//   }
+
+//   render() {
+//     const face = getFace(this.state);
+
+
+//     // return (
+//     //   <>
+//     //     <Controls actions={controlActions} handleSelection={this.handleSelection}/>
+//     //     <Face emoji={face} />
+//     //   </>
+//     // );
+//   }
+// }
